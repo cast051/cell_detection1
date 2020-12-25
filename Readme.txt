f3d8d8b7f1678b58b4805d7718b2c3a1d3fcfba4
liujinwei <411622401@qq.com>
2020-12-25 10:25:41 +0800

    code: 鳞状上皮细胞数量检测

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+time: 0.0523 
+Precious: 88.81     
+Recall: 87.24    
+F1_Measure: 88.02
+training   data :387
+validation data :67
+model:18_1.pb  size: 11.3M
---
+++ test.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from config import get_config
+import os
+from dataloader import dataloader
+from evalute import validation_model
+import numpy as np
+import cv2
+import glob
+import imageio
+
+os.environ['CUDA_VISIBLE_DEVICES']='4'
+
+
+def main():
+    is_training=False
+    #get config
+    config=get_config(is_training=is_training)
+
+    # load post process op
+    postprocess_module = tf.load_op_library(config.so_path)
+
+    # create session and load model
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    #load model
+    with tf.gfile.FastGFile(config.pb_path, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        input, output, net_output, info, get_info = tf.import_graph_def(graph_def,
+                      return_elements=["input:0", "output:0","net_output:0", "info:0","get_info:0"])
+
+    info_ = sess.run([info], feed_dict={get_info: 0})
+    print(info_)
+
+    if config.testmodel=="test1":
+        # load dataset
+        _, valid_records = dataloader.get_datasetlist(config.data_dir)
+        validation_dataset_reader = dataloader(valid_records, augument_flag=False)
+        validation_model(sess, valid_records, validation_dataset_reader, net_output, output, input,config.log_test_dir)
+
+    elif config.testmodel=="test2":
+        img_list = glob(config.test2_img_path + '/*.jpg')
+        for k, img_path in enumerate(img_list):
+            print(img_path)
+            path, name = os.path.split(img_path)
+            img_name = os.path.splitext(name)[0]
+            img = imageio.imread(img_path)
+            img = np.expand_dims(img, 0)
+            output_post, net_output_ = sess.run([output, net_output], feed_dict={input: img})
+            output_post = np.squeeze(output_post, 0)
+
+            # debug
+            print(len(output_post))
+            image_co = img[0].astype(np.uint8)
+            for j in range(output_post.shape[0]):
+                if output_post[j][2] == 1:
+                    pointx1 = output_post[j][0].astype(np.uint)
+                    pointy1 = output_post[j][1].astype(np.uint)
+                    cv2.circle(image_co, (pointx1, pointy1), 3, (0, 255, 0), -1)
+            imageio.imwrite(path + "/" + img_name + '_co.png', image_co)
+            imageio.imwrite(path + "/" + img_name + '_netout.png', (255 * net_output_[0]).astype(np.uint8))
+            print("Saved image: %d" % k)
+
+
+
+
+if __name__ == '__main__':
+    main()
